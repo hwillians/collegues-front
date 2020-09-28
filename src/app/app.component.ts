7fa8d0f4ce0500a8cd161d4882fa8fd27dfa8d33
Helvin <hjwc86@gmail.com>
2020-09-28 15:59:19 +0200

    Menu Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'collegues-front';
+  
+  links = [
+    { title: 'Accueil', fragment: '/accueil' },
+    { title: 'Gallerie', fragment: '/galerie' },
+    { title: 'A propos', fragment: '/apropos' }
+  ];
+
+  constructor(public route: ActivatedRoute) {}
 }
