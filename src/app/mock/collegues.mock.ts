eba067ae9667581eb921bb3e106b3f10ea8343f0
hwillians <helvin.willians@gmail.com>
2020-10-01 17:28:06 +0200

    mock of

--- src/app/mock/collegues.mock.ts
+++
@@ -1,9 +0,0 @@
-import { Collegue } from "../models/Collegues";
-
-
-// export const collegueMock: Collegue = new Collegue(' ', '  ', ' ', '  ', ' ', ' ')
-// export const c1: Collegue = new Collegue('XL0002', 'perez', 'leo', 'leo.perez@wowwow.fr', new Date(1600, 22, 1), 'https://i.ytimg.com/vi/sS4QydtwuRs/maxresdefault.jpg')
-// export const c2: Collegue = new Collegue('XL0003', 'goncalvez', 'helena', 'helena.goncalvez@wowwow.fr', new Date(1700, 3, 10), 'https://i.ytimg.com/vi/JSuW7HRbRzM/maxresdefault.jpg')
-// export const c3: Collegue = new Collegue('XL0004', 'hechevarria', 'sofia', 'sofi.hechez@wowwow.fr', new Date(2020, 31, 12), 'https://i.ytimg.com/vi/wdqJ03HcGVI/maxresdefault.jpg')
-
-// export const listMatricules: string[] = [collegueMock.matricule, c1.matricule, c2.matricule, c3.matricule]
